4a422c6cc395309ba57a6c9d0ffff6441dfff312
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-02-19 20:52:45 +0530

    nav added and gb added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ function App() {
 
   return (
     <>
-      <h1 className='text-white bg-slate-600'>Rushi Panchal</h1>
+      <header className="text-gray-400 bg-black body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0" href='#'>
+            <img src="fav-icon.png" alt="logo" className='size-11'/>
+          </a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-white " href='#'>about me</a>
+            <a className="mr-5 hover:text-white " href='#'>Projects</a>
+            <a className="mr-5 hover:text-white " href='#'>Contact</a>
+          </nav>
+        </div>
+      </header>
     </>
   )
 }
