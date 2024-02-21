7863bdce2a53fe0dd9483d4cbddcb5503e4a31e4
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-02-21 21:35:15 +0530

    add type writer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import './App.css'
+import TypeWritter from './TypeWritter';
 
 function App() {
+
+  // >> function for typewriter
+  // function typeWriter({text, delay}){
+  //   const [currentText, setCurrentText] = useState('');
+  //   const [currentIndex, setCurrentIndex] = useState(0);
+
+  //   useEffect(()=>{
+  //     let timeout;
+  //     if(currentIndex <= text.length){
+  //       timeout = setTimeout(() => {
+  //         setCurrentText(prevText => prevText+text[currentIndex]);
+  //         setCurrentIndex(prevIndex => prevIndex+1);
+  //       }, delay);
+
+  //       return ()=>clearTimeout(timeout);
+  //     }else if (infinite) { 
+  //       setCurrentIndex(0);
+  //       setCurrentText('');
+  //     }
+  //   }, [currentIndex, delay, infinite, text]);
+
+  //   return <span>{currentText}</span>
+  // }
 
   return (
     <>
@@ -23,9 +47,22 @@ function App() {
         <div id="section-01" className=' h-screen flex items-center'>
           <div className="AboutMe flex flex-row items-center ml-auto mr-auto">
             <img src="src/assets/gif-0.gif" alt="photo-gif" className='rounded-full size-96'/>
-            <span id='para'>
-              <p className='text-3xl text-white ml-3'>hello</p>
-            </span>
+            <div id='para' className='absolute right-6 text-wrap size-96'>
+              <h1 className='text-wrap text-white ml-3'>
+                - <TypeWritter text="I've been coding for a year now and have gained knowledge in HTML, CSS, JavaScript, React.js, Bootstrap, and R3F." delay={100} infinite />
+              </h1>
+              {/* - I've been coding for a year now and have gained knowledge in HTML, CSS, JavaScript, React.js, Bootstrap, and R3F. */}
+              <br />
+              <h1 className='text-wrap text-white ml-3'>
+                hello
+              </h1>
+              {/* - I want to learn more about R3F websites & Software Development(C++, Js). */}
+              <br />
+              <h1 className='text-wrap text-white ml-3'>
+                hello
+              </h1>
+              {/* - I enjoy playing and video games [Chess](https://www.chess.com/member/panchalrushi) in my free time. */}
+            </div>
           </div>
         </div>
         <div id="section-02" className='bg-blue-500 h-screen'>
