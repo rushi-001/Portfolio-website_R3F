3f7879532f477ee668b267020697c564f144285a
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-02-28 00:43:56 +0530

    logo link updated

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a
             className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
-            href="#"
+            href="index.html"
           >
             <img src="fav-icon.png" alt="logo" className="size-11" />
           </a>
