a0ee302b2895e87070124c9b55121372fdecb195
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-03-01 16:08:50 +0530

    Update CSS and JSX files. (generated by blackbox.ai )

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
       
         <div id="section-00" className=" lg:h-screen lg:flex h-full md:snap-center">
         {/* <MatrixRain numColumns="30" speed={100} /> */}
-        <CanvasAnimation />
+          <CanvasAnimation />
         </div>
 
         <div id="section-01" className="lg:h-screen lg:flex h-full flex items-center snap-center scroll-area">
@@ -176,8 +176,8 @@ function App() {
         <div id="section-02" className="lg:h-screen lg:flex h-full snap-center flex-wrap scroll-area text-gray-400 bg-gray-900 body-font">
             <div className="container px-5 py-24 mx-auto flex flex-wrap">
               <div className="lg:w-2/3 mx-auto">
-                <div className="flex flex-wrap w-full bg-gray-800 py-32 px-10 relative mb-4">
-                  <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="src/assets/cover-img1.png" />
+                <div className="flex flex-wrap w-full bg-gray-800 py-32 px-10 relative mb-4 overflow-hidden">
+                  <img alt="gallery" className="hover:scale-105 transition-transform duration-300 w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="src/assets/cover-img1.png" />
                   <div className="text-center relative z-10 w-full">
                     <h2 className="text-2xl text-white font-medium title-font mb-2">{"Wallpapers(ReactAPP)"}</h2>
                     <p className="leading-relaxed">This application allows users to search for photos using the Pixabay API.</p>
@@ -196,8 +196,8 @@ function App() {
 
                 <div className="flex flex-wrap -mx-2">
                   <div className="px-2 w-1/2">
-                    <div className="flex flex-wrap w-full bg-gray-800 sm:py-24 py-16 sm:px-10 px-6 relative">
-                      <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="src/assets/cover-img2.png" />
+                    <div className="flex flex-wrap w-full bg-gray-800 sm:py-24 py-16 sm:px-10 px-6 relative overflow-hidden">
+                      <img alt="gallery" className="hover:scale-105 transition-transform duration-300 w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="src/assets/cover-img2.png" />
                       <div className="text-center relative z-10 w-full">
                         <h2 className="text-xl text-white font-medium title-font mb-2">Quote Finder</h2>
                         <p className="leading-relaxed">Displays a random quotes using API & more.</p>
@@ -216,8 +216,8 @@ function App() {
                   </div>
 
                   <div className="px-2 w-1/2">
-                    <div className="flex flex-wrap w-full bg-gray-800 sm:py-24 py-16 sm:px-10 px-6 relative">
-                      <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="src/assets/cover-img3.png" />
+                    <div className="flex flex-wrap w-full bg-gray-800 sm:py-24 py-16 sm:px-10 px-6 relative overflow-hidden">
+                      <img alt="gallery" className="hover:scale-105 transition-transform duration-300 w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="src/assets/cover-img3.png" />
                       <div className="text-center relative z-10 w-full">
                         <h2 className="text-xl text-white font-medium title-font mb-2">Todo List</h2>
                         <p className="leading-relaxed">Users can add, delete, and mark tasks as completed.</p>
@@ -242,9 +242,49 @@ function App() {
 
         </div>
 
-        <div id="section-03" className="bg-green-500 h-screen snap-center scroll-area">
-          <h1>blue</h1>
+        <div id="section-03" className="lg:h-screen lg:flex h-full snap-center scroll-area justify-center">
+          <div className="container flex items-center text-center justify-center flex-col p-5">
+            <h1 className="text-white text-2xl">
+              <TypeWritter text="These platforms allows you to stay updated on me." />
+            </h1>
+            <span className="mt-5 flex flex-row">
+              <a href="https://www.linkedin.com/in/rushi-panchal-b58730239/">
+                <img src="src/assets/linkedin-icon.png" alt="linkedIn-icon" className="transition-transform duration-300 transform hover:scale-110 h-20 w-20 m-3" />
+              </a>
+              <a href="https://www.linkedin.com/in/rushi-panchal-b58730239/">
+                <img src="src/assets/x-icon.png" alt="linkedIn-icon" className="transition-transform duration-300 transform hover:scale-110 h-20 w-20 m-3"/>
+              </a>
+              <a href="https://www.linkedin.com/in/rushi-panchal-b58730239/">
+                <img src="src/assets/instagram-icon.png" alt="linkedIn-icon" className="transition-transform duration-300 transform hover:scale-110 h-20 w-20 m-3"/>
+              </a>
+              <a href="https://www.linkedin.com/in/rushi-panchal-b58730239/">
+                <img src="src/assets/github-icon.png" alt="linkedIn-icon" className="transition-transform duration-300 transform hover:scale-110 h-20 w-20 m-3"/>
+              </a>
+            </span>
+
+          </div>
         </div>
+
+
+        <footer className="text-gray-400 bg-black body-font body-font">
+          <div className="bg-black">
+            <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+              <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                <img src="src/assets/avtar-bg-yellow.png" alt="logo" viewBox="0 0 24 24" fill="none" className="w-10 h-10 text-white rounded-full" />
+                <span className="text-gray-400 ml-3 text-xl">Rushi Panchal</span>
+              </a>
+              <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2024 Rushi Panchal —
+                <a href="https://twitter.com/RUSHI76270899" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@RUSHI76270899</a>
+              </p>
+              <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                <a href="mailto:panchalrushi2182004@gmail.com" className="text-gray-500">
+                  <img src="src/assets/email-icon.png" alt="logo" viewBox="0 0 24 24" fill="none" className="w-10 h-10 text-white" />
+                </a>
+              </span>
+            </div>
+          </div>
+        </footer>
+
       </div>
     </>
   );
--- src/CanvasAnimation.jsx
+++ src/CanvasAnimation.jsx
@@ -13,13 +13,13 @@ const CanvasAnimation = () => {
 
         const chinese = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-*/?.,<>~`!@#$%^&()+=";
         // const chinese = "01";
-        const font_size = 12;
+        const font_size = 10;
         const columns = canvas.width / font_size;
         const drops = Array.from({ length: columns }, () => 1);
         const text = "Hello";
 
         const draw = () => {
-            cxt.fillStyle = "rgba(0,0,0,0.12";
+            cxt.fillStyle = "rgba(0,0,0,0.10";
             cxt.fillRect(0, 0, canvas.width, canvas.height);
 
             cxt.fillStyle = "#0F0";
@@ -45,7 +45,7 @@ const CanvasAnimation = () => {
     }, []);
 
     return (
-        <div style={{ position: 'relative' }}>
+        <div className='h-screen'>
             <canvas ref={canvasRef}></canvas>
             {/* <h1 className='text-yellow-400 z-40 text-5xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>Hello! 👋<br/>I'm Rushi.</h1> */}
             <GlitchedText />
