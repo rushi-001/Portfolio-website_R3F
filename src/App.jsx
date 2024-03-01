5483f3eea083f525fa49a694b0002fe751d9b52b
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-03-01 12:47:31 +0530

    section 02 updated
    add gallery in section

--- src/App.jsx
+++ src/App.jsx
@@ -60,14 +60,13 @@ function App() {
 
       <div id="body " className="!snap-y scroll snap-mandatory !scroll-smooth">
       
-        <div id="section-00" className=" h-screen md:snap-center">
+        <div id="section-00" className=" lg:h-screen lg:flex h-full md:snap-center">
         {/* <MatrixRain numColumns="30" speed={100} /> */}
         <CanvasAnimation />
         </div>
 
-        <div id="section-01" className="h-screen flex items-center snap-center scroll-area">
-          
-          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center lg:flex-grow ">
+        <div id="section-01" className="lg:h-screen lg:flex h-full flex items-center snap-center scroll-area">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center lg:flex-grow">
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
               <div className="lg:flex lg:flex-col lg:items-center">
                 <img
@@ -174,8 +173,55 @@ function App() {
           </div>
         </div>
 
-        <div id="section-02" className="bg-blue-500 h-screen snap-center scroll-area">
-          <h1>blue</h1>
+        <div id="section-02" className=" lg:h-screen lg:flex h-full snap-center flex-wrap scroll-area text-gray-400 bg-gray-900 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap">
+              <div className="lg:w-2/3 mx-auto">
+                <div className="flex flex-wrap w-full bg-gray-800 py-32 px-10 relative mb-4">
+                  <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/820x340" />
+                  <div className="text-center relative z-10 w-full">
+                    <h2 className="text-2xl text-white font-medium title-font mb-2">Shooting Stars</h2>
+                    <p className="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
+                    <a className="mt-3 text-indigo-300 inline-flex items-center">Learn More
+                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-2">
+                  <div className="px-2 w-1/2">
+                    <div className="flex flex-wrap w-full bg-gray-800 sm:py-24 py-16 sm:px-10 px-6 relative">
+                      <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x460" />
+                      <div className="text-center relative z-10 w-full">
+                        <h2 className="text-xl text-white font-medium title-font mb-2">Shooting Stars</h2>
+                        <p className="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
+                        <a className="mt-3 text-indigo-300 inline-flex items-center">Learn More
+                          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7" />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="px-2 w-1/2">
+                    <div className="flex flex-wrap w-full bg-gray-800 sm:py-24 py-16 sm:px-10 px-6 relative">
+                      <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x420" />
+                      <div className="text-center relative z-10 w-full">
+                        <h2 className="text-xl text-white font-medium title-font mb-2">Shooting Stars</h2>
+                        <p className="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
+                        <a className="mt-3 text-indigo-300 inline-flex items-center">Learn More
+                          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7" />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
         </div>
 
         <div id="section-03" className="bg-green-500 h-screen snap-center scroll-area">
